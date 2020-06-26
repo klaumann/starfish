5f3252916844a4b8f9bb5115864ff6242bae2a64
Malte Klaumann <malte@klaumann.org>
2020-06-26 17:54:26 +0200

    Created upload functionality client side

--- Public/javascript/admin.js
+++ Public/javascript/admin.js
@@ -13,3 +13,31 @@ function confirmDelete(path, id) {
     xmlHttp.send(null);
   }
 }
+
+function chooseImage() {
+  document.getElementById('imageDelete').value = false;
+  document.getElementById('image').click();
+}
+
+function removeImage() {
+  document.getElementById('image').value = null;
+  document.getElementById('imageDelete').value = true;
+  const element = document.getElementById('uploaded-image');
+  if (element !== null) {
+    element.parentNode.removeChild(element);
+  }
+}
+
+document.getElementById('image').onchange = function(event) { 
+  const file = event.target.files[0];
+  const blobURL = URL.createObjectURL(file);
+  let element = document.getElementById('uploaded-image');
+  if ( element === null) {
+    var newElement = document.createElement("img");
+    newElement.id = 'uploaded-image';
+    const sibling = document.getElementById('choose-button');
+    sibling.parentNode.insertBefore(newElement, sibling);
+    element = newElement;
+  }
+  element.src = blobURL;
+}
